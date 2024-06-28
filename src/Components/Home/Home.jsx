92a4ab12ccc2125a34765cde8cd4d6c78bbbb2bd
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-06-28 05:16:42 +0200

    added mb

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Hero from "../Hero/Hero";
+import Service from "../Service/Service";
 
 const Home = () => {
     return (<div>
 <Hero />
+<div className="mb-80"> 
+    hello my g
+</div>
 
     </div>  );
 }
