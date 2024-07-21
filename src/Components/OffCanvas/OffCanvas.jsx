e088a97af322b83f73d8adea66610a58019db863
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-21 02:28:41 +0200

    dark mode state added

--- src/Components/OffCanvas/OffCanvas.jsx
+++ src/Components/OffCanvas/OffCanvas.jsx
@@ -3,12 +3,25 @@ import { Link } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 import { theme } from '../../theme'; // Importing theme variables
 import './OffCanvas.css'; // Import the CSS file for animation
+import { useState } from 'react';
 
 const OffCanvas = ({ isMenuOpen, closeMenu }) => {
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () =>{
+        setDarkMode(!darkMode);
+    }
+
+
     const scrollTo = (id) => {
         document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
         closeMenu();
     };
+
+
+
+
 
     return (
         <>
@@ -24,6 +37,7 @@ const OffCanvas = ({ isMenuOpen, closeMenu }) => {
                 <FaTimes className="text-2xl cursor-pointer mb-6 self-end mr-4 mt-4" onClick={closeMenu} />
                 <Link to="/" className="py-2" onClick={closeMenu}>Home</Link>
                 <button className="py-2" onClick={() => scrollTo('content')}>Content</button>
+                <button onClick={toggleDarkMode}>{darkMode ? "dark" : "light"}</button>
             </div>
         </>
     );
