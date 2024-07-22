b6879551697bedd56a1b55fd2db2e4458e4f9009
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-22 02:47:42 +0200

    added icons

--- src/Components/Tools/modules/toolData.js
+++ src/Components/Tools/modules/toolData.js
@@ -1,11 +1,17 @@
 // toolData.js
 import image1 from "../../../assets/icons/Tools/reactjs.svg";
+import reactIcon from "../../../assets/icons/Tools/reactjs.svg";
+import jsIcon from "../../../assets/icons/Tools/js.svg";
+import tsIcon from "../../../assets/icons/Tools/ts.svg";
+import gitIcon from "../../../assets/icons/Tools/git.svg";
+
+
 
 const toolData = [
-  { imgSrc: image1, text: "React js" },
-  { imgSrc: image1, text: "HTML" },
-  { imgSrc: image1, text: "CSS" },
-  { imgSrc: image1, text: "JavaScript" },
+  { imgSrc: reactIcon, text: "React js" },
+  { imgSrc: jsIcon, text: "HTML" },
+  { imgSrc: tsIcon, text: "CSS" },
+  { imgSrc: gitIcon, text: "JavaScript" },
   { imgSrc: image1, text: "GIT" },
   { imgSrc: image1, text: "Tailwind CSS" },
   { imgSrc: image1, text: "WordPress/Elementor" },
