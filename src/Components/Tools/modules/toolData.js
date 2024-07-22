2cf1fc125dfa72b7910599d43720bba5fe3bcc6c
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-22 03:03:01 +0200

    added icons

--- src/Components/Tools/modules/toolData.js
+++ src/Components/Tools/modules/toolData.js
@@ -4,18 +4,22 @@ import reactIcon from "../../../assets/icons/Tools/reactjs.svg";
 import jsIcon from "../../../assets/icons/Tools/js.svg";
 import tsIcon from "../../../assets/icons/Tools/ts.svg";
 import gitIcon from "../../../assets/icons/Tools/git.svg";
+import htmlIcon from "../../../assets/icons/Tools/html.svg";
+import tailwindIcon from "../../../assets/icons/Tools/tailwind.svg";
+import nodeIcon from "../../../assets/icons/Tools/node.svg";
+import mongoIcon from "../../../assets/icons/Tools/mongo.svg";
 
 
 
 const toolData = [
   { imgSrc: reactIcon, text: "React js" },
-  { imgSrc: jsIcon, text: "HTML" },
-  { imgSrc: tsIcon, text: "CSS" },
-  { imgSrc: gitIcon, text: "JavaScript" },
-  { imgSrc: image1, text: "GIT" },
-  { imgSrc: image1, text: "Tailwind CSS" },
-  { imgSrc: image1, text: "WordPress/Elementor" },
-  { imgSrc: image1, text: "Shopify" },
+  { imgSrc: jsIcon, text: "JavaScript" },
+  { imgSrc: tsIcon, text: "TypeScript" },
+  { imgSrc: gitIcon, text: "GIT" },
+  { imgSrc: htmlIcon, text: "HTML" },
+  { imgSrc: tailwindIcon, text: "Tailwind CSS / CSS" },
+  { imgSrc: nodeIcon, text: "Node js" },
+  { imgSrc: mongoIcon, text: "MongoDB" },
   // Add more tools as needed
 ];
 
