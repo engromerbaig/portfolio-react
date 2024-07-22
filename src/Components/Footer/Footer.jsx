49b244befb65355d6c54624fe203190f8569199f
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-23 01:19:33 +0200

    more footer settings added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,20 @@
 import { links } from "../Navbar/modules/links";
 
 const Footer = () => {
-    return ( <div className="grid grid-col md:grid-cols-3 items-center justify-center">
-        <div>
-            <h1>About Me</h1>
+    return ( 
+        // parent for full footer
+    <div>
+
+    {/* parent for main footer */}
+    <div className="grid grid-col md:grid-cols-3 items-center justify-center border-t-2 border-b-2 border-gray-200 px-10 py-14">
+    <div className="flex flex-col gap-4">
+    <h1 className="text-2xl">About</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quaerat vero corrupti, mollitia soluta eaque voluptatum ex praesentium cupiditate accusamus quasi? Exercitationem maiores soluta enim natus commodi doloribus quidem quis?</p>
         </div>
 
-        <div>
-        <h1>Links</h1>
+        <div className="flex flex-col gap-4">
+      
+        <h1 className="text-2xl">Links</h1>
 
         {links.map((link, index) => (
         <p
@@ -20,8 +26,8 @@ const Footer = () => {
       ))}       
         </div>
 
-        <div>
-            <h1>Contact</h1>
+        <div className="flex flex-col gap-4">
+        <h1 className="text-2xl">Contact</h1>
             <p>omerbaigde@gmail.com</p>
             <p>Hamburg/Germany</p>
             <p>+4915217143817</p>
@@ -32,7 +38,18 @@ const Footer = () => {
                 <p>upwork</p>
             </div>
         </div>
-    </div> );
+        </div> 
+
+
+      {/* site credits */}
+      <div className="flex justify-center items-center py-3">
+      Copyright © Muhammad Omer Baig 2024.
+
+      </div>
+
+
+    </div> 
+    );
 }
  
 export default Footer;
