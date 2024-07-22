dd1c422a01570a24c9c876717ff8ad82936f6aa8
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-23 01:07:01 +0200

    added links

--- src/Components/Navbar/modules/links.js
+++ src/Components/Navbar/modules/links.js
@@ -1,7 +1,6 @@
 export const links = [
-    { label: 'Heading1', to: '/about-us' },
-    { label: 'Heading2', to: '/features' },
-    { label: 'Heading3', to: '/resources' },
-    { label: 'Heading4', to: '/pricing' },
-    { label: 'Heading5', to: '/contact-us' }
+    { label: 'Home', to: '/' },
+    { label: 'Contact', to: '/contact' },
+    { label: 'Projects', to: '/projects' },
+
 ];
