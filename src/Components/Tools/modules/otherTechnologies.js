acdf0d1d24cbf2e5d9c58b3c86363e237881e2a1
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-22 03:40:56 +0200

    added logos

--- src/Components/Tools/modules/otherTechnologies.js
+++ src/Components/Tools/modules/otherTechnologies.js
@@ -4,6 +4,8 @@ import wpIcon from "../../../assets/icons/Tools/Other/wp.svg";
 import elementorIcon from "../../../assets/icons/Tools/Other/elementor.svg";
 import shopifyIcon from "../../../assets/icons/Tools/Other/shopify.svg";
 import figmaIcon from "../../../assets/icons/Tools/Other/figma.svg";
+import seoIcon from "../../../assets/icons/Tools/Other/seo.svg";
+import dmIcon from "../../../assets/icons/Tools/Other/dm.svg";
 
 
 
@@ -12,8 +14,8 @@ const otherTechnologies = [
     { imgSrc: elementorIcon, text: "Elementor" },
     { imgSrc: shopifyIcon, text: "Shopify" },
     { imgSrc: figmaIcon, text: "Figma" },
-    { imgSrc: image1, text: "GIT" },
-    { imgSrc: image1, text: "Tailwind CSS" },
+    { imgSrc: seoIcon, text: "SEO/SEM" },
+    { imgSrc: dmIcon, text: "Digital Marketing" },
     // Add more technologies as needed
   ];
 
