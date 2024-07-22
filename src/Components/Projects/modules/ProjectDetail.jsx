57319a352f87436fa2c0bfbdd7cf8e445c6f15f4
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-22 04:10:20 +0200

    more styles

--- src/Components/Projects/modules/ProjectDetail.jsx
+++ src/Components/Projects/modules/ProjectDetail.jsx
@@ -3,7 +3,7 @@ import placeholderImage from '../../../assets/images/placeholder.png'
 const ProjectDetail = () => {
     return (    <div className="flex flex-col md:flex-row p-10 items-center justify-center">
         <img src={placeholderImage} alt="" className='w-1/3 aspect-square' />
-        <div className='flex w-1/2 gap-10 flex-col items-start justify-center shadow-lg p-8	'>
+        <div className='flex w-1/2 gap-10 flex-col items-start justify-center shadow-2xl rounded-md	 p-8	'>
             <h1 className=''>heading</h1>
             <p>The owners of Zekra realized that social media isn't enough, since they aren't getting enough sales. Our web solution certainly helped them grow online.</p>
             <div className="flex flex-row gap-10">
