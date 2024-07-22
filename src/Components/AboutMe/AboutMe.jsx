ec02e978531b895569d82111e427ced0412e1d1a
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-22 06:54:47 +0200

    added read more logic

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,20 +1,37 @@
+import React, { useState } from 'react';
 import omerImage from '../../assets/images/omer.jpg';
 import Heading from '../Heading/Heading';
 
-
 const AboutMe = () => {
-    return ( <div className="grid md:grid-cols-2 py-16 px-16">
-        <div className="flex justify-center p-8 ">
-        <img src={omerImage} alt="Hero" className="w-full lg:w-1/2 aspect-auto shadow-aboutme-image" />
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleText = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const shortText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus distinctio tempore, cupiditate pariatur debitis magnam?";
+    const fullText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus distinctio tempore, cupiditate pariatur debitis magnam? Tenetur recusandae pariatur dolor, esse minus expedita itaque vero sint dignissimos qui ad, delectus laudantium.";
+
+    return (
+        <div className="grid md:grid-cols-2 py-16 px-16">
+            <div className="flex justify-center p-8">
+                <img src={omerImage} alt="Hero" className="w-full lg:w-1/2 aspect-auto shadow-aboutme-image" />
+            </div>
+
+            <div className="flex flex-col px-12">
+                <Heading title="About Me, I'm Muhammad Omer Baig" />
+                <p>
+                    {isExpanded ? fullText : shortText}
+                </p>
+                <button
+                    onClick={toggleText}
+                    className="mt-4 text-theme-blue hover:underline text-left"
+                >
+                    {isExpanded ? 'Show Less' : 'Read More'}
+                </button>
+            </div>
         </div>
-
-        <div className="flex flex-col px-12">
-        <Heading title="About Me, I'm Muhammad Omer Baig" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus distinctio tempore, cupiditate pariatur debitis magnam? Tenetur recusandae pariatur dolor, esse minus expedita itaque vero sint dignissimos qui ad, delectus laudantium.</p>
-
-        </div>
-
-    </div> );
+    );
 }
- 
+
 export default AboutMe;
