6e289e8ad9343932419aee40e3c470396d5858ad
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-22 19:25:12 +0200

    added

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -1,6 +1,16 @@
+import Heading from "../Heading/Heading";
+import Button from "../Button/Button";
+
 const Message = () => {
-    return ( <div>
-        Send a Message
+    return ( <div className="w-full bg-theme-blue text-center py-20 px-20">
+        <Heading 
+        title="What's holding you back? Send me a message!"
+
+        />
+        <Button
+        text="Contact Us"
+        to="/contact-us"
+        />
     </div> );
 }
  
