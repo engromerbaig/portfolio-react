64d8bec3d7b9edaac09fd1fd44b932313a857829
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-22 02:31:45 +0200

    created toolData

--- src/Components/Tools/Tools.jsx
+++ src/Components/Tools/Tools.jsx
@@ -1,8 +1,8 @@
 // Tools.js
+import React from "react";
 import Heading from "../Heading/Heading";
 import ToolIcon from "../ToolIcon/ToolIcon";
-
-import image1 from "../../assets/icons/reactjs.svg";
+import { toolData, otherTechnologies } from "./toolData"; // Ensure the import path is correct
 
 const Tools = () => {
   return (
@@ -15,15 +15,9 @@ const Tools = () => {
       </div>
 
       <div className="grid grid-cols-2 gap-4 place-content-center sm:grid-cols-4 px-24 py-10">
-        <ToolIcon imgSrc={image1} text="React JS" />
-        <ToolIcon imgSrc={image1} text="HTML" />
-        <ToolIcon imgSrc={image1} text="CSS" />
-        <ToolIcon imgSrc={image1} text="JavaScript" />
-        <ToolIcon imgSrc={image1} text="GIT" />
-        <ToolIcon imgSrc={image1} text="Tailwind CSS" />
-        <ToolIcon imgSrc={image1} text="WordPress/Elementor" />
-        <ToolIcon imgSrc={image1} text="Shoxxpify" />
-        {/* Add more ToolIcon components as needed */}
+        {toolData.map((tool, index) => (
+          <ToolIcon key={index} imgSrc={tool.imgSrc} text={tool.text} />
+        ))}
       </div>
 
       <div className="text-center mb-8">
@@ -33,18 +27,10 @@ const Tools = () => {
       </div>
 
       <div className="flex flex-wrap justify-center gap-4 px-24 py-10">
-  <ToolIcon imgSrc={image1} text="Reacxt JS" />
-  <ToolIcon imgSrc={image1} text="HTML" />
-  <ToolIcon imgSrc={image1} text="CSS" />
-  <ToolIcon imgSrc={image1} text="JavaScript" />
-  <ToolIcon imgSrc={image1} text="GIT" />
-  <ToolIcon imgSrc={image1} text="Tailwind CSS" />
-</div>
-
-
-
-
-
+        {otherTechnologies.map((tech, index) => (
+          <ToolIcon key={index} imgSrc={tech.imgSrc} text={tech.text} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Components/Tools/toolData.js
@@ -0,0 +1,26 @@
+// toolData.js
+import image1 from "../../assets/icons/reactjs.svg";
+
+const toolData = [
+  { imgSrc: image1, text: "React JS" },
+  { imgSrc: image1, text: "HTML" },
+  { imgSrc: image1, text: "CSS" },
+  { imgSrc: image1, text: "JavaScript" },
+  { imgSrc: image1, text: "GIT" },
+  { imgSrc: image1, text: "Tailwind CSS" },
+  { imgSrc: image1, text: "WordPress/Elementor" },
+  { imgSrc: image1, text: "Shopify" },
+  // Add more tools as needed
+];
+
+const otherTechnologies = [
+  { imgSrc: image1, text: "React JS" },
+  { imgSrc: image1, text: "HTML" },
+  { imgSrc: image1, text: "CSS" },
+  { imgSrc: image1, text: "JavaScript" },
+  { imgSrc: image1, text: "GIT" },
+  { imgSrc: image1, text: "Tailwind CSS" },
+  // Add more technologies as needed
+];
+
+export { toolData, otherTechnologies };
