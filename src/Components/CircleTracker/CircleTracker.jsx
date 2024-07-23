ddb4541cbb25bf3eaaa72e56bb467010c2b5eb4e
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-23 08:43:48 +0200

    tracker working

--- src/Components/CircleTracker/CircleTracker.jsx
+++ src/Components/CircleTracker/CircleTracker.jsx
@@ -1,38 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
 const CircleTracker = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [circlePosition, setCirclePosition] = useState({ x: 0, y: 0 });
+  const [lerpFactor, setLerpFactor] = useState(0.2);
   useEffect(() => {
-    let animationFrameId;
-
     const handleMouseMove = (event) => {
-      setPosition({
+      setCursorPosition({
         x: event.clientX,
         y: event.clientY,
       });
     };
 
-    const updatePosition = () => {
-      animationFrameId = requestAnimationFrame(() => {
-        updatePosition();
-      });
-    };
-
     window.addEventListener('mousemove', handleMouseMove);
-    updatePosition();
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
-      cancelAnimationFrame(animationFrameId);
     };
   }, []);
+
+  useEffect(() => {
+    const lerp = (a, b, t) => a + (b - a) * t;
+
+    const newCirclePosition = {
+      x: lerp(circlePosition.x, cursorPosition.x, lerpFactor),
+      y: lerp(circlePosition.y, cursorPosition.y, lerpFactor),
+    };
+
+    setCirclePosition(newCirclePosition);
+  }, [cursorPosition, lerpFactor]);
 
   return (
     <div
       style={{
-        left: `${position.x}px`,
-        top: `${position.y}px`,
+        left: `${circlePosition.x}px`,
+        top: `${circlePosition.y}px`,
         pointerEvents: 'none', // Prevents the circle from interfering with mouse events
       }}
       className="fixed w-5 h-5 border-4 border-theme-blue rounded-full transform -translate-x-1/2 -translate-y-1/2"
@@ -40,4 +42,4 @@ const CircleTracker = () => {
   );
 };
 
-export default CircleTracker;s
+export default CircleTracker;
