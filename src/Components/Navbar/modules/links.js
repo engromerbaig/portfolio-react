8a534ba728a44dbc8420628be752f1b781c7c9af
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-23 05:14:21 +0200

    icons added

--- src/Components/Navbar/modules/links.js
+++ src/Components/Navbar/modules/links.js
@@ -1,8 +1,10 @@
 // src/components/Navbar/modules/links.js
-import { FaHome, FaEnvelope, FaProjectDiagram } from 'react-icons/fa';
+import { FaHome} from 'react-icons/fa';
+import { GrContact, GrProjects } from "react-icons/gr";
+
 
 export const links = [
     { label: 'Home', to: '/', icon: FaHome },
-    { label: 'Contact', to: '/contact', icon: FaEnvelope },
-    { label: 'Projects', to: '/projects', icon: FaProjectDiagram },
+    { label: 'Contact', to: '/contact', icon: GrContact },
+    { label: 'Projects', to: '/projects', icon: GrProjects },
 ];
