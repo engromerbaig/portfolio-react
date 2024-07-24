cd9f7e59a5ba162cf2eb6ac4b852ad0860d437c7
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-24 09:54:57 +0200

    variant animation kind of works

--- src/Components/Tools/modules/ToolSection.jsx
+++ src/Components/Tools/modules/ToolSection.jsx
@@ -1,5 +1,28 @@
+import React from "react";
+import { motion } from "framer-motion";
 import Heading from "../../Heading/Heading";
 import ToolIcon from "./ToolIcon";
+
+// Animation variants
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
 
 const ToolSection = ({ title, text, tools }) => {
   return (
@@ -8,11 +31,19 @@ const ToolSection = ({ title, text, tools }) => {
         title={title}
         text={text}
       />
-      <div className="grid grid-cols-2 gap-x-10 gap-y-12 place-content-center md:grid-cols-4 px-10 lg:px-24 py-10">
+      
+      <motion.div
+        className="grid grid-cols-2 gap-x-10 gap-y-12 place-content-center md:grid-cols-4 px-10 lg:px-24 py-10"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
         {tools.map((tool, index) => (
-          <ToolIcon key={index} imgSrc={tool.imgSrc} text={tool.text} />
+          <motion.div key={index} variants={item}>
+            <ToolIcon imgSrc={tool.imgSrc} text={tool.text} />
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
