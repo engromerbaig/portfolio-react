6401f4f6096d1e9633fa5ff7e5f2ee062c196e31
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-25 01:34:43 +0200

    added timer for  statsic item

--- src/Components/Statistics/modules/StatisticItem.jsx
+++ src/Components/Statistics/modules/StatisticItem.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import { FaCode, FaUsers, FaEarthAmericas } from 'react-icons/fa6';
 
 const iconStyles = "text-3xl aspect-square text-theme-blue motion-safe:animate-pulse";
@@ -12,32 +13,38 @@ const iconMap = {
 
 const StatisticItem = ({ title, iconType, value }) => {
   const [displayValue, setDisplayValue] = useState(0);
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.8, // Trigger only when the element is fully visible
+  });
+
   const Icon = iconMap[iconType] || FaCode;
 
   useEffect(() => {
-    const duration = 2; // Duration in seconds
-    const start = 0;
-    const end = value;
-    let startTime = null;
+    if (inView) {
+      const duration = 2; // Duration in seconds
+      const start = 0;
+      const end = value;
+      let startTime = null;
 
-    const animate = (timestamp) => {
-      if (!startTime) startTime = timestamp;
-      const progress = (timestamp - startTime) / (duration * 1000);
-      const current = Math.min(start + (end - start) * progress, end);
+      const animate = (timestamp) => {
+        if (!startTime) startTime = timestamp;
+        const progress = (timestamp - startTime) / (duration * 1000);
+        const current = Math.min(start + (end - start) * progress, end);
 
-      setDisplayValue(Math.floor(current));
+        setDisplayValue(Math.floor(current));
 
-      if (progress < 1) {
-        requestAnimationFrame(animate);
-      }
-    };
+        if (progress < 1) {
+          requestAnimationFrame(animate);
+        }
+      };
 
-    requestAnimationFrame(animate);
-
-  }, [value]);
+      requestAnimationFrame(animate);
+    }
+  }, [inView, value]);
 
   return (
-    <div className='flex flex-col gap-4'>
+    <div ref={ref} className='flex flex-col gap-4'>
       <h1 className='font-semibold'>{title}</h1>
       <div className="flex items-center justify-center gap-2">
         <Icon className={iconStyles} />
