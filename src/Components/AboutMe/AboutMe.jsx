99604c76882ce41017b1263e9568aa055c7ae2d1
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-24 02:27:21 +0200

    added descrption

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -3,9 +3,10 @@ import omerImage from '../../assets/images/omer2.jpg';
 import Heading from '../Heading/Heading';
 import SocialRow from '../Footer/modules/SocialRow';
 import { theme } from '../../theme';
+import { myDescription } from './myDescription';
 
 const AboutMe = () => {
-  const fullText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus distinctio tempore, cupiditate pariatur debitis magnam? Tenetur recusandae pariatur dolor, esse minus expedita itaque vero sint dignissimos qui ad, delectus laudantium. cupiditate pariatur debitis magnam? Tenetur recusandae pariatur dolor, esse minus expedita itaque vero sint dignissimos qui ad, delectus laudantium";
+  const combinedDescription = `${myDescription.text}\n${myDescription.additionalText}`;
 
   return (
     <div className={`grid md:grid-cols-2 gap-10 py-24 ${theme.sectionPaddings.horizontalPx}`} >
@@ -15,9 +16,9 @@ const AboutMe = () => {
 
       <div className="flex flex-col gap-6 py-14 px-4 ">
           <p className="h-full">
-          <Heading title="About Me, I'm Muhammad Omer Baig" />
+          <Heading title={myDescription.title} />
 
-            {fullText}
+            {combinedDescription}
 
           </p>
           {/* social row now */}
