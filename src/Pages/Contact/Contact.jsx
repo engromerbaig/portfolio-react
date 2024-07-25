bb316275de0c8b917cfb5e40429277af35d6fac9
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-25 10:09:23 +0200

    added dark models in routes

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,5 +1,7 @@
+import { theme } from "../../theme";
+
 const Contact = ({darkMode}) => {
-    return ( <div>
+    return ( <div className={`py-24 overflow-hidden ${theme.sectionBg.bgColor} ${theme.sectionBg.textColor}`}>
         this is contact page
     </div> );
 }
--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,7 +1,7 @@
-
+import { theme } from "../../theme";
 
 const Project = ({darkMode}) => {
-    return ( <div className="py-80">
+    return ( <div className={`py-24 overflow-hidden ${theme.sectionBg.bgColor} ${theme.sectionBg.textColor}`}>
         this is Project page
     </div> );
 }
