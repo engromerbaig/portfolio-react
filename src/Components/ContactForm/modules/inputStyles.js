ccbf8c8557195dc0ecd9a2e47d99bb97ec3dbfc4
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-26 03:45:05 +0200

    added some styles

--- src/Components/ContactForm/modules/inputStyles.js
+++ src/Components/ContactForm/modules/inputStyles.js
@@ -1,12 +1,12 @@
 // inputStyles.js
 const inputStyle = `
   w-full
-  rounded-lg
+  rounded-sm
   border-2
   border-form-border
   bg-form-field
   <px-4></px-4>
-  py-2
+  py-4
   focus:outline-none
   focus:ring-1
   focus:ring-theme-blue
