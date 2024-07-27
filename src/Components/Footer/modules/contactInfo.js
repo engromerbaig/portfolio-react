712442eddd11e243471187fbbf7a5904abe42f50
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 08:04:49 +0200

    created contactInfo

---
+++ src/Components/Footer/modules/contactInfo.js
@@ -0,0 +1,20 @@
+export const contactInfo = [
+{
+title:"Email",
+address:"omerbaigde@gmail.com",
+},
+
+{
+title:"Location",
+address:"Hamburg, Germany",
+},
+
+    
+{
+title:"Call Now",
+address:"+4915217143817",
+},
+        
+
+
+]
