e93406f582a2805c9836bbc64d2778596cec0139
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 14:16:29 +0200

    created job description module

---
+++ src/Components/JobDescription/JobDescription.jsx
@@ -0,0 +1,11 @@
+const JobDescription = () => {
+    return ( <div>
+        <h1>title here</h1>
+        <p>MY SKILLS</p>
+        <p>LANGUAGES USED</p>
+        <button>Buy Now</button>
+
+    </div> );
+}
+ 
+export default JobDescription;
