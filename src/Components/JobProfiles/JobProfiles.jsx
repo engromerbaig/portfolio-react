89d8fc0cebaf782de348f773583ef38907d9eb5b
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 16:05:57 +0200

    padding added

--- src/Components/JobProfiles/JobProfiles.jsx
+++ src/Components/JobProfiles/JobProfiles.jsx
@@ -19,7 +19,7 @@ const JobProfiles = ({ selectedTab, setSelectedTab }) => {
       </div>
 
       {/* Job Description */}
-      <div className="p-4">
+      <div className="py-4 px-10">
         <JobDescription profile={jobProfiles[selectedTab]} />
       </div>
     </div>
