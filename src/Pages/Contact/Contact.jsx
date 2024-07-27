21ccbad86c585407d2cc5712e8db26c805721268
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 07:55:41 +0200

    contact field created

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -29,6 +29,23 @@ const Contact = ({darkMode}) => {
         {/* Forms here now */}
 
 
+        {/* now contact fields */}
+
+        <div className="flex justify-center w-full ">
+        <div className="flex flex-col gap-4 md:flex-row items-start justify-center">
+        <h1 className="text-theme-blue font-bold">title </h1>
+        <div className="flex flex-col">
+        <h1>header</h1>
+        <p>email</p>
+
+        </div>
+
+        </div>
+
+
+        </div>
+
+
         </FadeInSection>
       
 
