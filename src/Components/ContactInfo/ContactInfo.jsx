471c7f3d2bedf0fe51984a2e6641a42ee2048fb6
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 08:21:17 +0200

    added links

--- src/Components/ContactInfo/ContactInfo.jsx
+++ src/Components/ContactInfo/ContactInfo.jsx
@@ -2,24 +2,26 @@ import React from "react";
 
 function ContactInfo({ contactInfo }) {
   return (
-    <div className="flex justify-center w-full">
-      <div className="flex flex-col gap-4 md:flex-row items-start justify-center">
-        {contactInfo.map((info, index) => (
-          <div key={index} className="flex flex-col items-start">
+    <div className="grid grid-cols-2 justify-center w-full px-80 gap-8">
+      {contactInfo.map((info, index) => (
+        <React.Fragment key={index}>
+          {/* lhs - Heading */}
+          <div className="flex flex-col items-start">
             <h1 className="text-theme-blue font-bold text-start">{info.heading}</h1>
-            <div className="flex flex-col">
-              <h2>{info.title}</h2>
-              {info.link ? (
-                <a href={info.link} target="_blank" rel="noopener noreferrer">
-                  {info.address}
-                </a>
-              ) : (
-                <p>{info.address}</p>
-              )}
-            </div>
           </div>
-        ))}
-      </div>
+          {/* rhs - Title and Address */}
+          <div className="flex flex-col items-start">
+            <h2>{info.title}</h2>
+            {info.link ? (
+              <a href={info.link} target="_blank" rel="noopener noreferrer">
+                {info.address}
+              </a>
+            ) : (
+              <p>{info.address}</p>
+            )}
+          </div>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
