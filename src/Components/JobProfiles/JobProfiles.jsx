60041a0759cc872c0f90305e1eacf7750e3d7fb5
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 15:42:47 +0200

    updated

--- src/Components/JobProfiles/JobProfiles.jsx
+++ src/Components/JobProfiles/JobProfiles.jsx
@@ -11,7 +11,7 @@ const JobProfiles = ({ selectedTab, setSelectedTab }) => {
           <button
             key={index}
             onClick={() => setSelectedTab(index)}
-            className={`px-4 py-2 border-2 mx-2 ${selectedTab === index ? 'bg-theme-blue text-white' : 'bg-light-hover dark:bg-dark-hover text-body-text-light dark:text-body-text-dark'}`}
+            className={`px-4 py-2  mx-2 ${selectedTab === index ? 'bg-theme-blue text-white' : 'bg-light-hover dark:bg-dark-hover text-body-text-light dark:text-body-text-dark'}`}
           >
             {profile.title}
           </button>
