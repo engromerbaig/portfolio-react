8124337cdcaa8811cd64ab20632e547745c4d832
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 16:29:04 +0200

    updated

--- src/Components/JobDescription/JobDescription.jsx
+++ src/Components/JobDescription/JobDescription.jsx
@@ -9,7 +9,7 @@ const JobDescription = ({ profile }) => {
     <div>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
         <div className={`flex flex-col h-32 ${sharedClass}`}>
-          <h1 className='font-bold'>
+          <h1 className='font-bold text-theme-blue'>
             <a href={profile.companyLink} target="_blank" rel="noopener noreferrer">
               {profile.company}
             </a>
