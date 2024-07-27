8cb03c8106160eba6328449c77387b6fd95872d7
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-27 08:13:13 +0200

    added links

--- src/Components/Footer/modules/contactInfo.js
+++ src/Components/Footer/modules/contactInfo.js
@@ -1,20 +1,21 @@
 export const contactInfo = [
-{
-title:"Email",
-address:"omerbaigde@gmail.com",
-},
-
-{
-title:"Location",
-address:"Hamburg, Germany",
-},
-
-    
-{
-title:"Call Now",
-address:"+4915217143817",
-},
-        
-
-
-]
+    {
+      heading: "Get in touch",
+      link: "mailto:omerbaigde@gmail.com", // Use the actual address here
+      title: "Email",
+      address: "omerbaigde@gmail.com",
+    },
+    {
+      heading: "Location",
+      link: "https://maps.app.goo.gl/tyL4yECBPzA1HG3G9",
+      title: "Based In",
+      address: "Hamburg, Germany",
+    },
+    {
+      heading: "Contact Directly",
+      link: "https://wa.me/4915217143817", // Use the actual address here
+      title: "Call Now",
+      address: "+4915217143817",
+    },
+  ];
+  
