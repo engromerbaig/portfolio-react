4aaa93b3063968a4387064861331d88e1e5f580f
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-28 15:26:05 +0200

    updated bg

--- src/utilities/Loaders/PreLoader/PreLoader.jsx
+++ src/utilities/Loaders/PreLoader/PreLoader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './PreLoader.css'
 
 const PreLoader = () => {
-  return     <div className="loader-container">
+  return     <div className="loader-container bg-theme-light dark:bg-theme-dark">
   <div className="loader"></div>
 </div>
 };
