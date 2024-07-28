ecd8aa872cff043236794b5aaf4c3cc74d3b6ce1
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-28 03:51:50 +0200

    updated

--- src/Components/JobProfiles/JobProfiles.jsx
+++ src/Components/JobProfiles/JobProfiles.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import { jobProfiles } from './modules/jobProfiles';
 import JobDescription from '../JobDescription/JobDescription';
 import SlideWrapper from '../../utilities/Animations/SlideWrapper';
+import FadeWrapper from '../../utilities/Animations/FadeWrapper';
 
 const JobProfiles = ({ selectedTab, setSelectedTab }) => {
   return (
     <div>
       {/* Tab Buttons */}
-        <SlideWrapper>
+      <SlideWrapper>
         <div className="flex justify-center mb-4">
-        {jobProfiles.map((profile, index) => (
-          <button
-            key={index}
-            onClick={() => setSelectedTab(index)}
-            className={`px-4 py-2  mx-2 ${selectedTab === index ? 'bg-theme-blue text-white' : 'bg-light-hover dark:bg-dark-hover text-body-text-light dark:text-body-text-dark'}`}
-          >
-            {profile.company}
-          </button>
-        ))}
-      </div>
-        </SlideWrapper>
-   
+          {jobProfiles.map((profile, index) => (
+            <button
+              key={index}
+              onClick={() => setSelectedTab(index)}
+              className={`px-4 py-2 mx-2 ${selectedTab === index ? 'bg-theme-blue text-white' : 'bg-light-hover dark:bg-dark-hover text-body-text-light dark:text-body-text-dark'}`}
+            >
+              {profile.company}
+            </button>
+          ))}
+        </div>
+      </SlideWrapper>
 
       {/* Job Description */}
-      <div className="py-4 px-4 lg:px-10">
-        <JobDescription profile={jobProfiles[selectedTab]} />
-      </div>
+      <FadeWrapper key={selectedTab}>
+        <div className="py-4 px-4 lg:px-10">
+          <JobDescription profile={jobProfiles[selectedTab]} />
+        </div>
+      </FadeWrapper>
     </div>
   );
 };
--- src/Pages/WorkExperience/Work.jsx
+++ src/Pages/WorkExperience/Work.jsx
@@ -3,29 +3,24 @@ import { theme } from '../../theme';
 import JobProfiles from '../../Components/JobProfiles/JobProfiles';
 import Heading from '../../Components/Heading/Heading';
 import FadeInSection from '../../utilities/Animations/FadeInSection';
-import FadeWrapper from '../../utilities/Animations/FadeWrapper';
 
 const Work = () => {
   const [selectedTab, setSelectedTab] = useState(0); // Initialize state with the first tab (0)
 
   return (
     <FadeInSection>
-    <div className={`py-24 overflow-hidden ${theme.sectionBg.bgColor} ${theme.sectionBg.textColor} `}>
+      <div className={`py-24 overflow-hidden ${theme.sectionBg.bgColor} ${theme.sectionBg.textColor} `}>
         <div className={`${theme.sectionPaddings.horizontalPx} py-20 text-center `}>
+          <Heading
+            title="Work Experience"
+            text="With 3+ years of experience, I have worked for reputed companies in Pakistan and Germany, alongside freelance projects. I excel in WordPress development, front-end technologies, and UI/UX design. My work has improved website traffic, user engagement, and content management. I am adept at handling tasks both remotely and in-house, collaborating effectively with international teams."
+          />
+        </div>
 
-        <Heading title="Work Experience" text="With 3+ years of experience, I have worked for reputed companies in Pakistan and Germany, alongside freelance projects. I excel in WordPress development, front-end technologies, and UI/UX design. My work has improved website traffic, user engagement, and content management. I am adept at handling tasks both remotely and in-house, collaborating effectively with international teams." />
+        <JobProfiles selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+
       </div>
-
-      <FadeWrapper>
-      <JobProfiles selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-
-
-      </FadeWrapper>
-    </div>
-
-
     </FadeInSection>
-
   );
 };
 
