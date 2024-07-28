78ced56dbbdbb4eae07ad4c038eaf2bbcab79cad
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-28 02:15:15 +0200

    added work

--- src/Components/Navbar/modules/links.js
+++ src/Components/Navbar/modules/links.js
@@ -1,12 +1,14 @@
 // src/components/Navbar/modules/links.js
 import { FaHome} from 'react-icons/fa';
 import { GrContact, GrProjects } from "react-icons/gr";
+import { MdWorkOutline } from "react-icons/md";
+
 
 
 export const links = [
     { label: 'Home', to: '/', icon: FaHome },
     { label: 'Contact', to: '/contact', icon: GrContact },
     { label: 'Projects', to: '/projects', icon: GrProjects },
-    { label: 'Work Experience', to: '/work-experience', icon: GrProjects },
+    { label: 'Work Experience', to: '/work-experience', icon: MdWorkOutline },
 
 ];
