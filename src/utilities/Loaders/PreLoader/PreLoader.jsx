d0b9933a56b42135834b7f08a2021d7bbe16f6c4
Muhammad Omer Baig <engromerbaig@gmail.com>
2024-07-28 14:57:42 +0200

    preloader crated but doesnt work

--- src/utilities/Loaders/PreLoader/PreLoader.jsx
+++ src/utilities/Loaders/PreLoader/PreLoader.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './styles.css'
 
 const PreLoader = () => {
-  return <div className={styles.loader}></div>;
+  return     <div className="loader-container">
+  <div className="loader"></div>
+</div>
 };
 
 export default PreLoader;
